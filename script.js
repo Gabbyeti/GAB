524f35aa27d86d1be9ee4ec889509a91fca13c90
Gabbyeti <110855187+Gabbyeti@users.noreply.github.com>
2022-10-28 17:09:48 +0100

    Bring deals

---
+++ script.js
@@ -0,0 +1,7 @@
+const gabby = document.querySelector(".nav-toggle"),
+      Emma = document.querySelector(".nav-menu");
+
+       gabby.onclick =function(){
+        
+        Emma.classList.toggle("active-nav")
+    }
